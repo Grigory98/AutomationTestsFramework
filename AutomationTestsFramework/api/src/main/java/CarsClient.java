ef2263cbc3ce332d782294e2827d89c41b15f053
Grigory98 <yakimov_ga@mail.ru>
2024-02-03 22:25:29 +0400

    added methods to CarsClient

--- AutomationTestsFramework/api/src/main/java/CarsClient.java
+++ AutomationTestsFramework/api/src/main/java/CarsClient.java
@@ -1,2 +1,27 @@
+import base.Response;
+import dto.Car;
+
 public interface CarsClient {
+    Response<Car[]> getCars();
+
+    Response<Car> getCar(final int id);
+
+    Response<Car[]> getUsersCars(final int userId);
+
+    Response<Car> createCar(
+            final String engineType,
+            final String mark,
+            final String model,
+            final double price
+    );
+
+    Response<Car> updateCar(
+            final int id,
+            final String engineType,
+            final String mark,
+            final String model,
+            final double price
+    );
+
+    int deleteCar(final int carId);
 }
