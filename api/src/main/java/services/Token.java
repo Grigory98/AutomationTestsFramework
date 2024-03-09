9743de2e4859a24651aee0a2a896f3883fc8ddd9
Grigory98 <yakimov_ga@mail.ru>
2024-03-09 15:18:38 +0400

    send arguments via args

--- api/src/main/java/services/Token.java
+++ api/src/main/java/services/Token.java
@@ -1,7 +1,6 @@
 package services;
 
 import base.RequestImpl;
-import config.Constants;
 import dto.TokenMap;
 
 import java.util.LinkedHashMap;
@@ -10,8 +9,8 @@ import java.util.Map;
 public class Token {
     public static String authorization() {
         Map<String, Object> params = new LinkedHashMap<>();
-        params.put("username", Constants.LOGIN);
-        params.put("password", Constants.PASSWORD);
+        params.put("username", System.getProperty("username"));
+        params.put("password", System.getProperty("password"));
         TokenMap response = new RequestImpl<TokenMap>().post(null, "/login", params, TokenMap.class);
         return "Bearer " + response.getToken();
     }
