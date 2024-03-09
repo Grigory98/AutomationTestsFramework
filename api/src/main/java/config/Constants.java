8378e248fc625fbcbd0df9d900aa4e0110b6ae7b
Grigory98 <yakimov_ga@mail.ru>
2024-03-09 12:09:21 +0400

    added action

--- api/src/main/java/config/Constants.java
+++ api/src/main/java/config/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     public static final String HOST = "http://77.50.236.203";
     public static final String URL_DEV = HOST + ":4879";
     public static final String URL_MASTER = HOST + ":4880";
-    public static final String LOGIN = "your login";
-    public static final String PASSWORD = "your password";
+    public static final String LOGIN = "user@pflb.ru";
+    public static final String PASSWORD = "user";
 }
--- ui/src/main/java/core/ApplicationConfig.java
+++ ui/src/main/java/core/ApplicationConfig.java
@@ -2,7 +2,7 @@ package core;
 
 public class ApplicationConfig {
     public static final String URL = "https://mail.ru/";
-    public static final boolean HEADLESS_MODE = false;
-    public static final String USERNAME = "yourLogin@mail.ru";
-    public static final String PASSWORD = "yourPassword";
+    public static final boolean HEADLESS_MODE = true;
+    public static final String USERNAME = "sethsryjtyj@mail.ru";
+    public static final String PASSWORD = "az09az09703";
 }
