7b83eeac9cd7389aeca01d86e3f32d45abff4599
Grigory98 <yakimov_ga@mail.ru>
2024-03-12 09:15:49 +0400

    fix

--- ui/src/main/java/core/ApplicationConfig.java
+++ ui/src/main/java/core/ApplicationConfig.java
@@ -2,7 +2,7 @@ package core;
 
 public class ApplicationConfig {
     public static final String URL = "https://account.mail.ru/login";
-    public static final boolean HEADLESS_MODE = true;
+    public static final boolean HEADLESS_MODE = false;
     public static final String USERNAME = System.getProperty("uiusername");
     public static final String PASSWORD = System.getProperty("uipassword");
 }
